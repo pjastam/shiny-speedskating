fd0a473266def97451cf16418ab40b36609fee05
Piet Stam <pjastam@gmail.com>
2018-02-10 20:19:40 +0100

    R files

---
+++ global.R
@@ -0,0 +1,18 @@
+#Load subroutine to construct the graph
+source("functions/plot.R")
+
+#Load subroutine to adjust for difference in birth dates
+source("functions/adjust.R")
+
+#Load speed skaters personalia
+assign("skaters_id",readRDS(file = ("data/skaters_id.rds")))
+
+#Initialize the track distances
+distance <- data.frame(distance = c(500,1000,1500,3000))
+
+#Load the track times (for all speed skaters and all distances)
+for (k in 1:nrow(distance)) {
+  for (j in 1:nrow(skaters_id)) {
+    assign(paste0("skater_",skaters_id[j,1], "_", distance[k,]),readRDS(file = paste0("data/", "skater_", skaters_id[j,1], "_", distance[k,], ".rds")))
+  }
+}
---
+++ ui.R
@@ -0,0 +1,29 @@
+# ui.R
+
+shinyUI(fluidPage(
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("The graph shows the best monthly track times of junior speed skaters compared to those of professional speed skaters, adjusted for the difference in dates of birth."),
+      
+      selectInput("junior", "Choose your junior ice skater", with(skaters_id, split(id,name)), selected = "35474"),
+      
+      selectInput("senior", "Choose your senior ice skater", with(skaters_id, split(id,name)), selected = "14920"),
+      
+      checkboxInput("adjust", 'Adjust for difference in birth dates', TRUE),
+      
+      selectInput("distance", 
+                  label = "Choose a distance",
+                  choices = list(500, 1000, 1500, 3000),
+                  selected = 500),
+      
+      tags$div(em('You can find the source code of this demo app',
+      tags$a(href="https://bitbucket.org/stamonline/shiny-speedskating", "here!")))
+    ),
+    
+    mainPanel(
+      plotOutput("timesPlot")
+    )
+
+  )
+))
